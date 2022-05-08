564f529af2c125ae0196c2afcd9fae6be67e374d
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-08 11:03:00 +0600

    Create a table and 5 different greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting_1 = Message.create(greeting: "Hola amigo!")
+greeting_2 = Message.create(greeting: "Good morning!")
+greeting_3 = Message.create(greeting: "Nice to meet you!")
+greeting_4 = Message.create(greeting: "Buenas dias!")
+greeting_5 = Message.create(greeting: "What's up!")

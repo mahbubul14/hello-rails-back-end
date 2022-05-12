1c851499928de1bc2d3fe514b596bd12cacfdaf4
mahbubul14 <mahbub21alam2017@gmail.com>
2022-05-12 22:06:24 +0600

    Implement required changes suggestion

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,5 +2,10 @@ class Api::V1::MessagesController < ApplicationController
   def index
     @message = Message.all.sample(1)
     render json: { messages: @message }.to_json
+
+    @random = Message.order('RANDOM()').first
+    render json: {
+      message: @random.message
+    }.to_json
   end
 end
